a1825b5be46ae85ccbe6400edd0d879f9c460ac4
Abdul Ahad Javaid <abdulahad@192.168.10.31>
2025-07-15 16:42:03 +0500

    change functionality

--- src/com/functional/programming/LambdaBehindTheScreensRunner.java
+++ src/com/functional/programming/LambdaBehindTheScreensRunner.java
@@ -1,8 +1,9 @@
 package com.functional.programming;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 class EvenNumberPredicate implements Predicate<Integer> {
 
@@ -11,15 +12,37 @@ class EvenNumberPredicate implements Predicate<Integer> {
         return integer % 2 == 0;
     }
 }
+
+class printAny implements Consumer<Integer> {
+
+    @Override
+    public void accept(Integer integer) {
+        System.out.println(integer);
+    }
+}
+
+class CustomMapper implements Function<Integer,Integer> {
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer * integer;
+    }
+}
+
 public class LambdaBehindTheScreensRunner {
     public static void main(String[] args) {
 
-        List.of(23,43,34,45,36,48).stream().filter(e -> e %2 == 0).forEach(e -> System.out.println(e));
+        List.of(23,43,34,45,36,48).stream().filter(e -> e %2 == 0).map(e -> e*e).
+                forEach(e -> System.out.println(e));
 
        // Stream<T> filter(Predicate<? super T> predicate);
        // boolean test(T t);
+        // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
 
         List.of(23,43,34,45,36,48).stream().filter(new EvenNumberPredicate()).forEach(e -> System.out.println(e));
+        System.out.println("-".repeat(30));
+        List.of(23,43,34,45,36,48).stream().filter(new EvenNumberPredicate()).map(new CustomMapper()).forEach(new printAny());
+
 
     }
 }
