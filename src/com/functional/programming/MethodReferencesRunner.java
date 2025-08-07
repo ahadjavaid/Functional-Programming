7005920142769ff80534d4b1b7a4e3f3dd917e8c
Abdul Ahad Javaid <abdulahad@192.168.10.19>
2025-08-07 13:06:18 +0500

    new exercises

--- src/com/functional/programming/MethodReferencesRunner.java
+++ src/com/functional/programming/MethodReferencesRunner.java
@@ -26,7 +26,7 @@ public class MethodReferencesRunner {
                 forEach(MethodReferencesRunner::print);
 
         List.of("Apple","Banana","Kiwi","Mango").stream().map(String::length).forEach(MethodReferencesRunner::print);
-
+        System.out.println("-".repeat(30));
         Integer maxNumber = List.of(23,45,67,34).stream().filter(MethodReferencesRunner::even)
                 .max(Integer::compare).orElse(0);
         System.out.println(maxNumber);
--- src/com/functional/programming/codingexercises/CubeClass.java
+++ src/com/functional/programming/codingexercises/CubeClass.java
@@ -1,6 +1,8 @@
 package com.functional.programming.codingexercises;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -9,11 +11,39 @@ public class CubeClass {
         System.out.println(getCubeOfFirstNumbers(5));
         IntStream.range(1,11).map(e -> e * e).forEach(e -> System.out.println(e));
         System.out.println(IntStream.range(1,11).map(e -> e * e).boxed().collect(Collectors.toList()));
+        System.out.println(getCourseLengthCount(List.of()));
+        System.out.println(getSquare(List.of()));
     }
 
     public static List<Integer> getCubeOfFirstNumbers(int number) {
         List <Integer> cubeList = IntStream.range(1,number+1).map(e -> e * e * e).boxed()
                 .collect(Collectors.toList());
         return cubeList;
+    }
+
+    public static List<Integer> getCourseLengthCount(List<String> courseName) {
+        if(courseName == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> courseLengthList = courseName.stream().map(e -> e.length()).collect(Collectors.toList());
+        return courseLengthList;
+    }
+
+    public static long getSquare(List<Integer> numbers) {
+        if(numbers == null) {
+            return 0;
+        }
+
+        return numbers.stream().mapToLong(e -> e * e).reduce(0,(number1, number2) ->
+                number1 + number2);
+    }
+
+    public static int findMaxEvenNumber(List<Integer> numbers) {
+        if(numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+
+        Optional<Integer> maxEven = numbers.stream().filter(e -> e % 2 == 0).max(Integer::compare);
+        return maxEven.orElse(0);
     }
 }
